30f1753ac1bd33534bcbddc57d346a5824dddc23
Nikolay Nikolaev <66773789+Notarrogantjustbetter@users.noreply.github.com>
2021-05-16 22:05:46 +0300

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/Notarrogantjustbetter/CRUD/v2/database"
 	"github.com/Notarrogantjustbetter/CRUD/v2/utils"
@@ -14,9 +16,12 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		utils.ExecuteTemplate(w, "home.html", nil)
 	case "POST":
 		r.ParseForm()
-		name := r.PostForm.Get("Name")
-		email := r.PostForm.Get("Email")
-		database.CreateUser(name, email)
+		user := database.User{}
+		user.Name = r.PostForm.Get("Name")
+		err := database.PostgresDb{}.CreateUser(&user)
+		if err != nil {
+			fmt.Fprint(w, "Failed to create user")
+		}
 		http.Redirect(w, r, "/", http.StatusFound)
 	}
 }
@@ -27,8 +32,17 @@ func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
 		utils.ExecuteTemplate(w, "deleteUser.html", nil)
 	case "POST":
 		r.ParseForm()
+		user := database.User{}
 		id := r.PostForm.Get("ID")
-		database.DeleteUser(id)
+		strToInt, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			fmt.Fprint(w, "Failed to convert string to int")
+		}
+		user.ID = uint(strToInt)
+		err = database.PostgresDb{}.DeleteUser(&user)
+		if err != nil {
+			fmt.Fprint(w, "Failed to delete user")
+		}
 		http.Redirect(w, r, "/", http.StatusFound)
 	}
 }
@@ -39,16 +53,28 @@ func updateUserHandler(w http.ResponseWriter, r *http.Request) {
 		utils.ExecuteTemplate(w, "updateUser.html", nil)
 	case "POST":
 		r.ParseForm()
-		id := r.PostForm.Get("ID")
-		key := r.PostForm.Get("Key")
-		value := r.PostForm.Get("Value")
-		database.UpdateUser(id, key, value)
+		user := database.User{}
+		id := r.PostForm.Get("id")
+		user.Name = r.PostForm.Get("newName")
+		strToInt, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			fmt.Fprint(w, "Failed to convert id")
+		}
+		user.ID = uint(strToInt)
+		err = database.PostgresDb{}.UpdateUser(&user)
+		if err != nil {
+			fmt.Fprint(w, "Failed to update user")
+		}
 		http.Redirect(w, r, "/", http.StatusFound)
 	}
 }
 
 func getUsersHandler(w http.ResponseWriter, r *http.Request) {
-	utils.ExecuteTemplate(w, "users.html", database.GetUsers())
+	context, err := database.PostgresDb{}.GetAllUsers()
+	if err != nil {
+		fmt.Fprint(w, "Failed to get users")
+	}
+	utils.ExecuteTemplate(w, "users.html", context)
 }
 
 func InitRouter() *mux.Router {
